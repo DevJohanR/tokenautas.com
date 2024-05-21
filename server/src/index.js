8511176095340232f90cb9126a688f5b26252168
DevJohanR <desarrollador7777@gmail.com>
2024-05-20 20:12:00 -0500

    Rama Development: Test Servir Archivos Estaticos public_html

--- server/src/index.js
+++ server/src/index.js
@@ -1,21 +1,19 @@
-// server/src/index.js
-//test
 const express = require('express');
 const cors = require('cors');
 const userRoutes = require('./routes/userRoutes');
 const withdrawalsRoutes = require('./routes/withdrawalsRoutes'); // Importa las rutas de retiros
-
 const errorHandler = require('./middleware/errorHandler');
 const path = require('path');
-
-
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// Sirve los archivos estáticos de la carpeta 'assets' que está dentro de 'src'
+// Sirve los archivos estáticos de la carpeta 'public_html'
+app.use(express.static(path.join(__dirname, '../../public_html')));
+
+// Sirve los archivos estáticos de la carpeta 'assets' dentro de 'src'
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 app.use('/users', userRoutes);
@@ -23,5 +21,10 @@ app.use('/withdrawals', withdrawalsRoutes);
 
 // Middleware de manejo de errores global
 app.use(errorHandler);
+
+// Middleware para manejar las rutas de React
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public_html', 'index.html'));
+});
 
 module.exports = app;
