66a5dbd89444dceea43dda4e6512fc126bf23a77
DevJohanR <desarrollador7777@gmail.com>
2024-05-24 15:32:24 -0500

    Se actualizan Extensiones de los array de .png a .jpeg

--- server/src/config/arrays.js
+++ server/src/config/arrays.js
@@ -1,35 +1,35 @@
 //server/src/config/arrays.js
 const arraybtc = [
   {
-    imagen: 'assets/(NUMERO1)BTC1LLqNQoeFrQPJWDq9ctAF89nkFXtA5Nygq.png',
+    imagen: 'assets/(NUMERO1)BTC1LLqNQoeFrQPJWDq9ctAF89nkFXtA5Nygq.jpeg',
     palabraSecreta: '1LLqNQoeFrQPJWDq9ctAF89nkFXtA5Nygq',
   },
   {
-    imagen: 'assets/NUMERO5BTC1JvBVVCgQH36jqDe9dTKkbrELyNzoPTBKy.png',
+    imagen: 'assets/NUMERO5BTC1JvBVVCgQH36jqDe9dTKkbrELyNzoPTBKy.jpeg',
     palabraSecreta: '1JvBVVCgQH36jqDe9dTKkbrELyNzoPTBKy',
   },
   {
-    imagen: 'assets/NUMERO7BTC12sYzrtr48rJaCq8EGR7diRtswwmVU3H5L.png',
+    imagen: 'assets/NUMERO7BTC12sYzrtr48rJaCq8EGR7diRtswwmVU3H5L.jpeg',
     palabraSecreta: '12sYzrtr48rJaCq8EGR7diRtswwmVU3H5L',
   },
   {
-    imagen: 'assets/NUMERO9BTC1DgZBBeVFLfPQmqXkgTvcbVEwFoZd9kcHT.png',
+    imagen: 'assets/NUMERO9BTC1DgZBBeVFLfPQmqXkgTvcbVEwFoZd9kcHT.jpeg',
     palabraSecreta: '1DgZBBeVFLfPQmqXkgTvcbVEwFoZd9kcHT',
   },
   {
-    imagen: 'assets/NUMERO11BTC1NPnQPvYZfQtSWBp3iZzD1ANeRRjgW9DyT.png',
+    imagen: 'assets/NUMERO11BTC1NPnQPvYZfQtSWBp3iZzD1ANeRRjgW9DyT.jpeg',
     palabraSecreta: '1NPnQPvYZfQtSWBp3iZzD1ANeRRjgW9DyT',
   },
   {
-    imagen: 'assets/NUMERO13BTC19xCA2hnc762SspR2UR7czu45dLNtxVhrs.png',
+    imagen: 'assets/NUMERO13BTC19xCA2hnc762SspR2UR7czu45dLNtxVhrs.jpeg',
     palabraSecreta: '19xCA2hnc762SspR2UR7czu45dLNtxVhrs',
   },
   {
-    imagen: 'assets/NUMERO15BTC15FruFyJv2DnbqrGkj9gA1o2sDXGKMUBmT.png',
+    imagen: 'assets/NUMERO15BTC15FruFyJv2DnbqrGkj9gA1o2sDXGKMUBmT.jpeg',
     palabraSecreta: '15FruFyJv2DnbqrGkj9gA1o2sDXGKMUBmT',
   },
   {
-    imagen: 'assets/NUMERO17BTC1HDDa5vMaTC4VSo2XUeyAK6mCpKNbwpv9Q.png',
+    imagen: 'assets/NUMERO17BTC1HDDa5vMaTC4VSo2XUeyAK6mCpKNbwpv9Q.jpeg',
     palabraSecreta: '1HDDa5vMaTC4VSo2XUeyAK6mCpKNbwpv9Q',
   },
 ];
@@ -37,35 +37,35 @@ const arraybtc = [
 
 const arrayusdt = [
   {
-    imagen: 'assets/(NUMERO4)USDTTK8PxYPYmmUUr4UnmQxmXAE8kDBUCsgp8D.png',
+    imagen: 'assets/(NUMERO4)USDTTK8PxYPYmmUUr4UnmQxmXAE8kDBUCsgp8D.jpeg',
     palabraSecreta: 'TK8PxYPYmmUUr4UnmQxmXAE8kDBUCsgp8D',
   },
   {
-    imagen: 'assets/NUMERO2USDTTYpH9rHr1ve2kPySiCiofgmF7L9be2aD2d.png',
+    imagen: 'assets/NUMERO2USDTTYpH9rHr1ve2kPySiCiofgmF7L9be2aD2d.jpeg',
     palabraSecreta: 'TYpH9rHr1ve2kPySiCiofgmF7L9be2aD2d',
   },
   {
-    imagen: 'assets/NUMERO6USDTTHxCnBgTex9SB89SpNbRLLGREQhrxHb9RQ.png',
+    imagen: 'assets/NUMERO6USDTTHxCnBgTex9SB89SpNbRLLGREQhrxHb9RQ.jpeg',
     palabraSecreta: 'THxCnBgTex9SB89SpNbRLLGREQhrxHb9RQ',
   },
   {
-    imagen: 'assets/NUMERO8USDTTNpumi7rGi9TZQypayi3b5kRCHsPxRVW2F.png',
+    imagen: 'assets/NUMERO8USDTTNpumi7rGi9TZQypayi3b5kRCHsPxRVW2F.jpeg',
     palabraSecreta: 'TNpumi7rGi9TZQypayi3b5kRCHsPxRVW2F',
   },
   {
-    imagen: 'assets/NUMERO10USDTTJPKZLzA8qTsPKuSNx4P5Pjpgz4ZicHYcB.png',
+    imagen: 'assets/NUMERO10USDTTJPKZLzA8qTsPKuSNx4P5Pjpgz4ZicHYcB.jpeg',
     palabraSecreta: 'TJPKZLzA8qTsPKuSNx4P5Pjpgz4ZicHYcB',
   },
   {
-    imagen: 'assets/NUMERO12USDTTX3AzKjaJGwHeYH4KYi5PQ1XFQTmcB4DsF.png',
+    imagen: 'assets/NUMERO12USDTTX3AzKjaJGwHeYH4KYi5PQ1XFQTmcB4DsF.jpeg',
     palabraSecreta: 'TX3AzKjaJGwHeYH4KYi5PQ1XFQTmcB4DsF',
   },
   {
-    imagen: 'assets/NUMERO14USDTTPDiaR9tdQNAr8edfhwUXEDoRdo5qPEuuZ.png',
+    imagen: 'assets/NUMERO14USDTTPDiaR9tdQNAr8edfhwUXEDoRdo5qPEuuZ.jpeg',
     palabraSecreta: 'TPDiaR9tdQNAr8edfhwUXEDoRdo5qPEuuZ',
   },
   {
-    imagen: 'assets/NUMERO16USDTTEBcKKR4Ne5e8ojEemkDkUsz8KpJSreae6.png',
+    imagen: 'assets/NUMERO16USDTTEBcKKR4Ne5e8ojEemkDkUsz8KpJSreae6.jpeg',
     palabraSecreta: 'TEBcKKR4Ne5e8ojEemkDkUsz8KpJSreae6',
   },
 ];
