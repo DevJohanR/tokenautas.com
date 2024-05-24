ccd2fed7ffcba39c78a65742390008405b090f9f
DevJohanR <desarrollador7777@gmail.com>
2024-05-24 14:26:31 -0500

    se cambian QRs y Billeteras de prueba a Real

--- server/src/config/arrays.js
+++ server/src/config/arrays.js
@@ -1,92 +1,74 @@
 //server/src/config/arrays.js
 const arrayusdt = [
-    {
-      imagen: 'assets/usdt1.jpeg',
-      palabraSecreta: 'PalabraSecreta1-USDT',
-    },
-    {
-      imagen: 'assets/usdt2.jpeg',
-      palabraSecreta: 'PalabraSecreta2-USDT',
-    },
-    {
-      imagen: 'assets/usdt3.jpeg',
-      palabraSecreta: 'PalabraSecreta3-USDT',
-    },
-    {
-      imagen: 'assets/usdt4.jpeg',
-      palabraSecreta: 'PalabraSecreta4-USDT',
-    },
-    {
-      imagen: 'assets/usdt5.webp',
-      palabraSecreta: 'PalabraSecreta5-USDT',
-    },
-    {
-      imagen: 'assets/usdt6.webp',
-      palabraSecreta: 'PalabraSecreta6-USDT',
-    },
-    {
-      imagen: 'assets/usdt7.webp',
-      palabraSecreta: 'PalabraSecreta7-USDT',
-    },
-    {
-      imagen: 'assets/usdt8.webp',
-      palabraSecreta: 'PalabraSecreta8-USDT',
-    },
-    {
-      imagen: 'assets/usdt9.webp',
-      palabraSecreta: 'PalabraSecreta9-USDT',
-    },
-    {
-      imagen: 'assets/usdt10.webp',
-      palabraSecreta: 'PalabraSecreta10-USDT',
-    },
-    // ... (agrega más imágenes y palabras secretas para USDT)
-  ];
-  
-  const arraybtc = [
-    {
-      imagen: 'assets/btc1.jpeg',
-      palabraSecreta: 'PalabraSecreta1-BTC',
-    },
-    {
-      imagen: 'assets/btc2.jpeg',
-      palabraSecreta: 'PalabraSecreta2-BTC',
-    },
-    {
-      imagen: 'assets/btc3.jpeg',
-      palabraSecreta: 'PalabraSecreta3-BTC',
-    },
-    {
-      imagen: 'assets/btc4.jpeg',
-      palabraSecreta: 'PalabraSecreta4-BTC',
-    },
-    {
-      imagen: 'assets/btc5.jpg',
-      palabraSecreta: 'PalabraSecreta5-BTC',
-    },
-    {
-      imagen: 'assets/btc6.jpg',
-      palabraSecreta: 'PalabraSecreta6-BTC',
-    },
-    {
-      imagen: 'assets/btc7.jpg',
-      palabraSecreta: 'PalabraSecreta7-BTC',
-    },
-    {
-      imagen: 'assets/btc8.jpg',
-      palabraSecreta: 'PalabraSecreta8-BTC',
-    },
-    {
-      imagen: 'assets/btc9.jpg',
-      palabraSecreta: 'PalabraSecreta9-BTC',
-    },
-    {
-      imagen: 'assets/btc10.jpg',
-      palabraSecreta: 'PalabraSecreta10-BTC',
-    },
-    
-    // ... (agrega más imágenes y palabras secretas para BTC)
-  ];
+  {
+    imagen: 'assets/(NUMERO4) USDT TK8PxYPYmmUUr4UnmQxmXAE8kDBUCsgp8D.png',
+    palabraSecreta: 'TK8PxYPYmmUUr4UnmQxmXAE8kDBUCsgp8D',
+  },
+  {
+    imagen: 'assets/NUMERO 2 USDT TYpH9rHr1ve2kPySiCiofgmF7L9be2aD2d.png',
+    palabraSecreta: 'TYpH9rHr1ve2kPySiCiofgmF7L9be2aD2d',
+  },
+  {
+    imagen: 'assets/NUMERO6 USDT THxCnBgTex9SB89SpNbRLLGREQhrxHb9RQ.png',
+    palabraSecreta: 'THxCnBgTex9SB89SpNbRLLGREQhrxHb9RQ',
+  },
+  {
+    imagen: 'assets/NUMERO8 USDT TNpumi7rGi9TZQypayi3b5kRCHsPxRVW2F.png',
+    palabraSecreta: 'TNpumi7rGi9TZQypayi3b5kRCHsPxRVW2F',
+  },
+  {
+    imagen: 'assets/NUMERO10 USDT TJPKZLzA8qTsPKuSNx4P5Pjpgz4ZicHYcB.png',
+    palabraSecreta: 'TJPKZLzA8qTsPKuSNx4P5Pjpgz4ZicHYcB',
+  },
+  {
+    imagen: 'assets/NUMERO12 USDT TX3AzKjaJGwHeYH4KYi5PQ1XFQTmcB4DsF.png',
+    palabraSecreta: 'TX3AzKjaJGwHeYH4KYi5PQ1XFQTmcB4DsF',
+  },
+  {
+    imagen: 'assets/NUMERO14 USDT TPDiaR9tdQNAr8edfhwUXEDoRdo5qPEuuZ.png',
+    palabraSecreta: 'TPDiaR9tdQNAr8edfhwUXEDoRdo5qPEuuZ',
+  },
+  {
+    imagen: 'assets/NUMERO16 USDT TEBcKKR4Ne5e8ojEemkDkUsz8KpJSreae6.png',
+    palabraSecreta: 'TEBcKKR4Ne5e8ojEemkDkUsz8KpJSreae6',
+  },
+];
+
+const arraybtc = [
+  {
+    imagen: 'assets/(NUMERO1)BTC 1LLqNQoeFrQPJWDq9ctAF89nkFXtA5Nygq.png',
+    palabraSecreta: '1LLqNQoeFrQPJWDq9ctAF89nkFXtA5Nygq',
+  },
+  {
+    imagen: 'assets/NUMERO5 BTC 1JvBVVCgQH36jqDe9dTKkbrELyNzoPTBKy.png',
+    palabraSecreta: '1JvBVVCgQH36jqDe9dTKkbrELyNzoPTBKy',
+  },
+  {
+    imagen: 'assets/NUMERO7 BTC 12sYzrtr48rJaCq8EGR7diRtswwmVU3H5L.png',
+    palabraSecreta: '12sYzrtr48rJaCq8EGR7diRtswwmVU3H5L',
+  },
+  {
+    imagen: 'assets/NUMERO9 BTC 1DgZBBeVFLfPQmqXkgTvcbVEwFoZd9kcHT.png',
+    palabraSecreta: '1DgZBBeVFLfPQmqXkgTvcbVEwFoZd9kcHT',
+  },
+  {
+    imagen: 'assets/NUMERO11 BTC 1NPnQPvYZfQtSWBp3iZzD1ANeRRjgW9DyT.png',
+    palabraSecreta: '1NPnQPvYZfQtSWBp3iZzD1ANeRRjgW9DyT',
+  },
+  {
+    imagen: 'assets/NUMERO13 BTC 19xCA2hnc762SspR2UR7czu45dLNtxVhrs.png',
+    palabraSecreta: '19xCA2hnc762SspR2UR7czu45dLNtxVhrs',
+  },
+  {
+    imagen: 'assets/NUMERO15 BTC 15FruFyJv2DnbqrGkj9gA1o2sDXGKMUBmT.png',
+    palabraSecreta: '15FruFyJv2DnbqrGkj9gA1o2sDXGKMUBmT',
+  },
+  {
+    imagen: 'assets/NUMERO17 BTC 1HDDa5vMaTC4VSo2XUeyAK6mCpKNbwpv9Q.png',
+    palabraSecreta: '1HDDa5vMaTC4VSo2XUeyAK6mCpKNbwpv9Q',
+  },
+];
+
   
   module.exports = {
     arrayusdt,
