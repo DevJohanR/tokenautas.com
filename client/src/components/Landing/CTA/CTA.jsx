33c9270bc254dccdb71237baff19438b31787cdc
DevJohanR <desarrollador7777@gmail.com>
2024-06-01 13:09:14 -0500

    se actualiza cta al español

--- client/src/components/Landing/CTA/CTA.jsx
+++ client/src/components/Landing/CTA/CTA.jsx
@@ -5,11 +5,11 @@ const CTA = () => (
   <div className="containerCTA">
   <div className="gpt3__cta">
     <div className="gpt3__cta-content">
-      <p>Request Early Access to Get Started</p>
-      <h3>Register Today & start exploring the endless possibilities.</h3>
+      <p>Registrate hoy y convierte tus tokens a pesos colombianos</p>
+      <h3>Regístrese hoy y comience a explorar las infinitas posibilidades.</h3>
     </div>
     <div className="gpt3__cta-btn">
-      <button type="button">Get Started</button>
+      <button type="button">Registrarme Ahora!</button>
     </div>
   </div>
   </div>
